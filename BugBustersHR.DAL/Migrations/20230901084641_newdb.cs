53906b1f6eff86d08f3f3f6d29609402afba71b1
Ersin Bahar <ersinbaharr@gmail.com>
2023-09-01 12:00:20 +0300

    newmigration

---
+++ BugBustersHR.DAL/Migrations/20230901084641_newdb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugBustersHR.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class newdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "0f6805a6-f83d-4b95-a248-7756edf0669b", "771aeda8-c47e-4397-ad24-fe6cf3d731c6" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "d02ec6e6-03ac-476f-abab-cf1afb0b8c18", "08a144cb-ab6b-4c08-9693-f67fe276991f" });
+        }
+    }
+}
